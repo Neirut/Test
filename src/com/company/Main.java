18d80f58c05913f797571b9f04af7b6d09377252
Oleg <tarasov_oleg@hotmail.com>
2022-07-14 16:28:50 +0300

    v.3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println();
-        for (int i = 0; i < ; i++) {
-            
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
         }
     }
 }
